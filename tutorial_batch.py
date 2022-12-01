7d6d60f7cb48a4f3357c04456c3857f6eed65551
Erez Hadad <erezh@il.ibm.com>
2022-12-01 15:03:36 +0200

    Added autoscaling of 1 to 3 replicas, UNTESTED

--- tutorial_batch.py
+++ tutorial_batch.py
@@ -8,7 +8,12 @@ import time
 import sys
 import click
 
-@serve.deployment(ray_actor_options={"num_cpus": 0, "num_gpus": 1})
+@serve.deployment(autoscaling_config={
+        "min_replicas": 1,
+        "max_replicas": 3,
+        "target_num_ongoing_requests_per_replica": 10,
+    }, 
+    ray_actor_options={"num_cpus": 0, "num_gpus": 1})
 class BatchTextGenerator:
     def __init__(self, use_gpu):
         torch_device = -1
